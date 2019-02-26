5a7265e821d3e728537ca0c2b309f682d6b8dfa7
David Young <davidy@funkypenguin.co.nz>
2019-02-26 15:21:44 +1300

    Update

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,4 @@
 alias reload!='. ~/.zshrc'
 
 # Occasionally (like when cloning VMs) it's necessary to ignore all the warnings from SSH about duplicate / mismatched keys
-alias insecuressh='ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
+alias insecuressh='ssh -oKexAlgorithms=+diffie-hellman-group1-sha1 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
